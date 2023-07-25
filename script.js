6f7684abca9ba09e56c59847a6a0d91e7f0d85ad
Luca <luca.ambrosonee@gmail.com>
2023-07-25 17:03:29 +0200

    Improved design on heading products, added flash sales w JS code and rounder heading div

--- script.js
+++ script.js
@@ -11,3 +11,42 @@ function moreReviews() {
         moreReviewsBox.style.display = "none";
         moreReviewsText.innerHTML = "▼ Leggi altre recensioni ▼"; }
 };
+
+// timer offerte lampo
+function startCountdown(duration, display) {
+    var timer = duration, hours, minutes, seconds;
+    setInterval(function () {
+        hours = parseInt(timer / 3600, 10);
+        minutes = parseInt((timer % 3600) / 60, 10);
+        seconds = parseInt(timer % 60, 10);
+
+        hours = hours < 10 ? "0" + hours : hours;
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+
+        display.textContent = hours + "h " + minutes + "m " + seconds + "s";
+        if (--timer < 0) {
+            // Timer scaduto, puoi gestire qui cosa succede dopo l'offerta lampo
+            // Ad esempio, nascondere l'offerta o mostrare un messaggio
+            display.textContent = "Scaduto!";
+        }
+    },1000);
+}
+
+// Avvia il timer con una durata di 3 ore, 20 minuti e 14 secondi
+var timerDuration = 3 * 60 * 60 + 23 * 60 + 14;
+var display = document.getElementById('countdown');
+startCountdown(timerDuration, display);
+
+function cambiaColore() {
+    var timer = document.getElementById("timer");
+    if (timer.classList.contains("red")) {
+        timer.classList.remove("red");
+        timer.classList.add("yellow");
+    } else {
+        timer.classList.remove("yellow");
+        timer.classList.add("red");
+    }
+}
+
+setInterval(cambiaColore, 500);
