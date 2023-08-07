cecd4ac9ecd2254c87b25b657d20ddf66605ce2b
Luca <luca.ambrosonee@gmail.com>
2023-08-07 16:55:41 +0200

    major update to cart popup

--- js/negozio.js
+++ js/negozio.js
@@ -194,86 +194,92 @@ function cartExpandSwitch() {
     }
 };
 
-var nItems = 0;
-var totalPrice;
-var nuovoPrezzo;
-// Funzione per aggiornare l'interfaccia del carrello
-function updateCartUI(prezzoTotale) {
-    console.log("prezzoTotale appena passato a updateCartUI " + prezzoTotale);
-    var carrelloDiv = document.getElementById("itemCartList");
-    var carrello = JSON.parse(localStorage.getItem("carrello")) || [];
+function clearCart() {
+    localStorage.removeItem("cart");
+    updateCartUI();
+    shoppingCart.style.display = "none";
+}
 
-    carrelloDiv.innerHTML = "";
+var cart = [];
+
+function updateCartUI() {
+    var cartDiv = document.getElementById("itemCartList");
+    cartDiv.innerHTML = "";
     var nItems = 0;
 
-    carrello.forEach(function (prodotto) {
-        var NewProductDiv = document.createElement("div");
-        NewProductDiv.className = "cartItem";
+    var groupedProducts = cart.reduce(function (acc, product) {
+        var found = acc.find(function (item) {
+            return item.name === product.name;
+        });
+        if (found) {
+            found.quantity += product.quantity;
+            found.price = (parseFloat(found.price) + parseFloat(product.price)).toFixed(2);
+        } else {
+            acc.push(product);
+        }
+        return acc;
+    }, []);
 
-        var prezzoProdotto = parseFloat(prodotto.prezzo);
-
-        NewProductDiv.innerHTML = `
-            <div class="tooltip" onclick="deleteItem()">
-                <h4 class="cartProductName">${prodotto.nome}</h4>
+    groupedProducts.forEach(function (product, index) {
+        var productPrice = parseFloat(product.price);
+        var newProductDiv = document.createElement("div");
+        newProductDiv.className = "cartItem";
+        newProductDiv.innerHTML = `
+            <div class="tooltip" onclick="removeItem(${index})">
+                <h4 class="cartProductName">${product.name}</h4>
                 <h3 class="cartProductQuantityAndPrice">
-                    <span>${prodotto.quantita}gr.ㅤ</span>
-                    <span>${prezzoProdotto.toFixed(2)}€</span>
+                    <span>${product.quantity}gㅤ</span>
+                    <span>${productPrice.toFixed(2)}€</span>
                 </h3>
-                <span class="tooltiptext">Rimuovi prodotto</span>
+                <span class="tooltiptext">Remove product</span>
             </div>
         `;
-
-        NewProductDiv.style.display = "flex";
-        carrelloDiv.appendChild(NewProductDiv);
-
+        newProductDiv.style.display = "flex";
+        cartDiv.appendChild(newProductDiv);
         nItems++;
     });
 
-    var cart = document.getElementById("carrello");
-    cart.innerHTML = "Carrello (" + nItems + " elementi):";
-    if (nItems > 0) {
-        var shoppingCart = document.getElementById("shoppingCart");
+    var cartCount = document.getElementById("cart");
+    cartCount.innerHTML = "Carrello (Prodotti: " + nItems + "):";
+    
+    if (nItems === 0) {
+        shoppingCart.style.display = "none";
+    } else {
         shoppingCart.style.display = "flex";
     }
 }
 
-function addToCart(product, cartProductName, Price) {
-    var itemCartList = document.getElementById("itemCartList");
+function addToCart(product, productName, price) {
     var quantity = product.querySelector(".quantity");
-    var quantitaValue = parseInt(quantity.value);
-    var cart = document.getElementById("carrello");
+    var quantityValue = parseInt(quantity.value);
 
-    if (quantitaValue < 100) {
-        return; // Esci se la quantità è inferiore a 100g
+    if (quantityValue > 0) {
+        var existingProduct = cart.find(function (item) {
+            return item.name === productName;
+        });
+
+        if (existingProduct) {
+            existingProduct.quantity += quantityValue;
+            existingProduct.price = (parseFloat(existingProduct.price) + (parseFloat(price) * quantityValue / 100)).toFixed(2);
+        } else {
+            var newProduct = {
+                name: productName,
+                quantity: quantityValue,
+                price: (parseFloat(price) * quantityValue / 100).toFixed(2)
+            };
+            cart.push(newProduct);
+        }
+
+        localStorage.setItem("cart", JSON.stringify(cart));
+        updateCartUI();
     }
+}
 
-    var prodotto = {
-        nome: cartProductName,
-        quantita: quantitaValue,
-        prezzo: (parseFloat(Price) * quantitaValue / 100).toFixed(2)
-    };
-
-    var carrello = JSON.parse(localStorage.getItem("carrello")) || [];
-
-    var prodottoPresente = carrello.find(function (item) {
-        return item.nome === cartProductName;
-    });
-
-    if (prodottoPresente) {
-        prodottoPresente.quantita = parseInt(prodottoPresente.quantita) + quantitaValue;
-        prodottoPresente.prezzo = (parseFloat(prodottoPresente.prezzo) + parseFloat(prodotto.prezzo)).toFixed(2);
-    } else {
-        carrello.push(prodotto);
-    }
-
-    localStorage.setItem("carrello", JSON.stringify(carrello));
+function removeItem(index) {
+    cart.splice(index, 1);
+    localStorage.setItem("cart", JSON.stringify(cart));
     updateCartUI();
 }
 
+cart = JSON.parse(localStorage.getItem("cart")) || [];
 updateCartUI();
-
-function emptyCart() {
-    localStorage.removeItem("carrello");
-    updateCartUI();
-    shoppingCart.style.display = "none";
-}
